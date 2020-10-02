b326af65c79aa49f4f51964cecb4936fc364aa6e
dimitriskoslek <dimitriskl@protonmail.com>
2020-10-02 04:00:20 +0300

    Init commit of basic template. SCSS added

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="testouter">
+		<p id="testinner">test</p>
+	</div>
+</main>
+
+<style lang="scss">
+	#testouter {
+		background-color: black;
+		height: 200px;
+
+		#testinner {
+			height: 100px;
+			background-color: red;
+		}
+	}
+
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
